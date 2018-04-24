f1d61040702725a9fb94a731edc6286dc8f60bb7
Deepjot <deepjot@D-10-16-28-140.dhcp4.washington.edu>
2018-04-24 15:17:25 -0700

    Added home, main, login

--- app/src/main/java/spr018/tcss450/clientapplication/LoginActivity.java
+++ app/src/main/java/spr018/tcss450/clientapplication/LoginActivity.java
@@ -1,34 +1,86 @@
 package spr018.tcss450.clientapplication;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.CheckBox;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.support.v4.app.FragmentTransaction;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import spr018.tcss450.clientapplication.utility.Pages;
 
 public class LoginActivity extends AppCompatActivity
         implements LoginFragment.OnFragmentInteractionListener,
         RegisterFragment.OnFragmentInteractionListener {
-
+    private Credentials mCredentials;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
+        if (savedInstanceState == null) {
 
-        loadFragment(new LoginFragment(), Pages.LOGIN);
+//            if(findViewById(R.id.fragmentContainer)!= null){
+//                SharedPreferences prefs = getSharedPreferences(
+//                        getString(R.string.keys_shared_prefs), Context.MODE_PRIVATE);
+//                if (prefs.getBoolean(getString(R.string.keys_prefs_stay_logged_in), false)) {
+//                    showMainActivity();
+//                } else {
+//
+//                    getSupportFragmentManager().beginTransaction().
+//                            add(R.id.loginFragmentContainer, new LoginFragment(),
+//                                    getString(R.string.keys_fragment_login)).
+//                            commit();
+//                    //loadFragment(new LoginFragment(), Pages.LOGIN);
+//                }
+//            }
+            SharedPreferences prefs = getSharedPreferences(
+                        getString(R.string.keys_shared_prefs), Context.MODE_PRIVATE);
+                if (prefs.getBoolean(getString(R.string.keys_prefs_stay_logged_in), false)) {
+                    showMainActivity();
+                    Log.d("CHECK IN WAS SAVED","SHARED PREFS");
+                } else {
+
+                    getSupportFragmentManager().beginTransaction().
+                            add(R.id.loginFragmentContainer, new LoginFragment(),
+                                    getString(R.string.keys_fragment_login)).
+                            commit();
+                    //loadFragment(new LoginFragment(), Pages.LOGIN);
+                }
+
+
+        }
+
 
     }
 
     @Override
-    public void onLoginInteraction(int type) {
+    public void onLoginInteraction(Credentials c, int type) {
         switch (type) {
             case 0:
-                showMainActivity();
+                //showMainActivity(); //USER HAS SUCCESSFULLY LOGGED IN
+                //step 51 in lab 4 track 1.
+                //build
+                Uri uri = new Uri.Builder().scheme("https").
+                        appendPath(getString(R.string.ep_base_url)).
+                        appendPath(getString(R.string.ep_login)).build();
+                //build
+                JSONObject msg = c.asJSONObject();
+                mCredentials = c;
+                Log.d("ONLOGININTERACTION","CREDENTIALS ASSIGNED TO C.");
+                new SendPostAsyncTask.Builder(uri.toString(), msg).
+                        onPostExecute(this::handleLoginOnPost)
+                        .onCancelled(this::handleErrorsInTask) .build().execute();
                 break;
             case 1:
                 loadFragment(new RegisterFragment(), Pages.REGISTER);
@@ -36,6 +88,55 @@ public class LoginActivity extends AppCompatActivity
             default:
                 Log.wtf("Impossible", "How did this happen?");
                 break;
+        }
+    }
+
+
+    private void checkStayLoggedIn(){
+            if(((CheckBox)findViewById(R.id.LogFragCheckBox)).isChecked()) {
+                Log.e("CHECK BOX", "CHECKED");
+                SharedPreferences p = getSharedPreferences
+                        (getString(R.string.keys_shared_prefs), Context.MODE_PRIVATE);
+                //Save the username for later usage
+                p.edit().putString(getString(R.string.keys_shared_prefs),
+                        mCredentials.getUsername()).apply();
+                //save the users "want" to stay logged in
+                p.edit().putBoolean(getString(R.string.keys_prefs_stay_logged_in), true).apply();
+            }
+
+    }
+
+
+
+
+
+    /**
+     * Handle errors that may occur during the AsyncTask.
+     * @param result the error message provide from the AsyncTask */
+    private void handleErrorsInTask(String result) {
+        Log.e("ASYNCT_TASK_ERROR", result);
+    }
+
+    /**
+     * Handle onPostExecute of the AsynceTask. The result from our webservice is * a JSON formatted String. Parse it for success or failure.
+     * @param result the JSON formatted String response from the web service
+     */
+    private void handleLoginOnPost(String result) {
+        try{
+            JSONObject resultsJSON = new JSONObject(result);
+            boolean success = resultsJSON.getBoolean("success");
+            Log.e("HANDLELOGINONPOST","TRYING SUCCESS");
+            if(success){
+                //login was successful so open the main activity
+                checkStayLoggedIn();
+                showMainActivity();
+            } else {
+                //login failed.
+                LoginFragment frag = (LoginFragment) getSupportFragmentManager().findFragmentByTag(getString(R.string.keys_fragment_login));
+                frag.setError("Log in unsuccessful");
+            }
+        } catch(JSONException e){
+            Log.e("JSON_PARSE_ERROR", result+System.lineSeparator()+e.getMessage());
         }
     }
 
--- app/src/main/java/spr018/tcss450/clientapplication/LoginFragment.java
+++ app/src/main/java/spr018/tcss450/clientapplication/LoginFragment.java
@@ -3,10 +3,13 @@ package spr018.tcss450.clientapplication;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 /**
@@ -44,9 +47,24 @@ public class LoginFragment extends Fragment {
         if (mListener != null) {
             int id = view.getId();
             if (id == R.id.loginButton) {
-                mListener.onLoginInteraction(0);
+                EditText username = getView().findViewById(R.id.LogFragUsername);
+                EditText password = getView().findViewById(R.id.LogFragPassword);
+                if(username.getText().toString().equals("")){
+                    username.setError("Fields cant be empty");
+                } if(password.getText().toString().equals("")){
+                    password.setError("Field cant be empty.");
+                } else {
+                    //send to the successful login screen.
+                    String u = username.getText().toString();
+
+                    Editable p = password.getText();
+                    Credentials c = new Credentials.Builder(u,p).build();
+                    mListener.onLoginInteraction(c,0);
+
+                }
+
             } else if (id == R.id.registerButton) {
-                mListener.onLoginInteraction(1);
+                //mListener.onLoginInteraction();
             }
         }
     }
@@ -60,6 +78,15 @@ public class LoginFragment extends Fragment {
             throw new RuntimeException(context.toString()
                     + " must implement OnFragmentInteractionListener");
         }
+    }
+
+    /**
+     * Allows an external source to set an error message on this fragment. This may * be needed if an Activity includes processing that could cause login to fail. * @param err the error message to display.
+     */
+    public void setError(String err) {
+        //Log in unsuccessful for reason: err. Try again.
+        //you may want to add error stuffs for the user here
+        ((TextView) getView().findViewById(R.id.LogFragUsername)).setError("Login Unsuccessul");
     }
 
     @Override
@@ -80,6 +107,6 @@ public class LoginFragment extends Fragment {
      */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
-        void onLoginInteraction(int type);
+        void onLoginInteraction(Credentials c, int type);
     }
 }
