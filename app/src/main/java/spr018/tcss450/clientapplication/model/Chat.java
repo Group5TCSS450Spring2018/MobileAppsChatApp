02d4bdc1de88ea238983c4f18e8bc90075e0ea9a
Tuan Dinh <tuandinhsemail@gmail.com>
2018-05-18 11:09:07 -0700

    implemented time parsing for chat and have recent chat sorted by most recent.

--- app/src/main/java/spr018/tcss450/clientapplication/model/Chat.java
+++ app/src/main/java/spr018/tcss450/clientapplication/model/Chat.java
@@ -2,8 +2,11 @@ package spr018.tcss450.clientapplication.model;
 
 import android.support.annotation.NonNull;
 
-import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * This custom connection class hold is used to hold all the information
@@ -13,7 +16,7 @@ public class Chat implements Comparable<Chat> {
 
     private String mName;
     private List<String> mMembers;
-    private String mTimestamp;
+    private Calendar mTimestamp;
     private int mChatId;
     private String mUsername;
     private String mRecentMessage;
@@ -21,23 +24,34 @@ public class Chat implements Comparable<Chat> {
     public Chat(String chatName, List<String> members, String timestamp, int chatID) {
         mName = chatName;
         mMembers = members;
-        mTimestamp = timestamp;
+        mTimestamp = parseTime(timestamp);
         mChatId = chatID;
     }
 
     public Chat(String chatName, String recentMessage, String timestamp, int chatID) {
         mName = chatName;
         mRecentMessage = recentMessage;
-        mTimestamp = timestamp;
+        mTimestamp = parseTime(timestamp);
         mChatId = chatID;
     }
 
     public Chat(String chatName, String chatRecentMessage, String timestamp, int chatID, String username) {
         mName = chatName;
         mRecentMessage = chatRecentMessage;
-        mTimestamp = timestamp;
+        mTimestamp = parseTime(timestamp);
         mChatId = chatID;
         mUsername = username;
+    }
+
+    private Calendar parseTime(String timestamp) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS", Locale.US);
+        Calendar calendar = Calendar.getInstance(Locale.US);
+        try {
+            calendar.setTime(format.parse(timestamp));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return calendar;
     }
 
     public String getName() {
@@ -48,7 +62,7 @@ public class Chat implements Comparable<Chat> {
         return mMembers;
     }
 
-    public String getTimestamp() {
+    public Calendar getTimestamp() {
         return mTimestamp;
     }
 
@@ -74,7 +88,7 @@ public class Chat implements Comparable<Chat> {
     }
 
     public void setTimestamp(String timestamp) {
-        mTimestamp = timestamp;
+        mTimestamp = parseTime(timestamp);
     }
 
     public void setChatID(int chatID) {
@@ -87,6 +101,6 @@ public class Chat implements Comparable<Chat> {
 
     @Override
     public int compareTo(@NonNull Chat connection) {
-        return Timestamp.valueOf(mTimestamp).compareTo(Timestamp.valueOf(connection.mTimestamp));
+        return connection.getTimestamp().compareTo(mTimestamp);
     }
 }
