b2bb47e56103a112daf14335b5f9e79d9ca02308
Tuan Dinh <tuandinhsemail@gmail.com>
2018-05-20 05:26:52 -0700

    modified 24 hours weather.

--- app/src/main/java/spr018/tcss450/clientapplication/HomeFragment.java
+++ app/src/main/java/spr018/tcss450/clientapplication/HomeFragment.java
@@ -1,15 +1,10 @@
 package spr018.tcss450.clientapplication;
 
-import android.Manifest;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -18,13 +13,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApi;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -76,7 +64,7 @@ public class HomeFragment extends Fragment{
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_home, container, false);
         mWeatherWidget = v.findViewById(R.id.weatherText);
-        mLocationWidget = v.findViewById(R.id.locationText);
+        mLocationWidget = v.findViewById(R.id.weatherTabCurrentLocation);
         mPrefs =
                 Objects.requireNonNull(getActivity()).getSharedPreferences(
                         getString(R.string.keys_shared_prefs),
